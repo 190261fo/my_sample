1beae6e8022ed42a0746cafd37b4631b32eca6be
190261fo <69778270+190261fo@users.noreply.github.com>
2020-12-09 10:30:38 +0900

    route_lesson

---
+++ Lesson_1209/routerLesson.js
@@ -0,0 +1,27 @@
+/* ルーティング*/
+// app.HTTPメソッド(パス, コールバック関数);
+/*
+app.get(関数);
+app.post(関数);
+app.put(関数);
+app.delete(関数);
+*/
+
+/* プロジェクト初期化*/
+// $ npm init
+
+/* Express をインストール*/
+// $ npm install express
+
+/* プログラム作成 -> server.js*/
+// モジュール読み込み
+// const express = require('express')
+// const router = express.Router()
+
+// ミドルウェア
+// app.use('/', router)
+
+// ルーティング
+// router.get('/', (req, res) => {
+//     res.send('Hello Express Router!!')
+// });
---
+++ express_router/server.js
@@ -0,0 +1,41 @@
+// モジュール読み込み
+// const express = require("express");
+var express = require("express");
+    //var 一番ゆるい定義, const ほぼ定数で再定義できない
+
+// Router作成
+const router = express.Router();
+
+// サーバ作成
+// const app = express();
+var app = express();
+
+// ミドルウェア router
+app.use("/", router);
+
+// ルーティング
+router.get("/", (req, res) => {
+    // res.send("Hello Express Router!!");
+    res.send("Route index!!!");
+});
+
+router.get("/profile", (req, res) => {
+    res.send("Tokyo Taro");
+});
+
+router.get("/calculate", (req, res) => {
+    var tax_rate = 0.1;
+    var price = req.query.price;
+    var tax = price * tax_rate;
+    res.send(String(tax));
+});
+
+
+// router.get("/message/list", (req, res) => {
+//     res.send("Message list page");
+// });
+
+// サーバ待機
+app.listen(3000);
+
+console.log("Listen: http://localhost:3000");
