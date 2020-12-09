4af05e13a4ac6f57ddb1ddac2cb191eaf6db0bf2
190261fo <69778270+190261fo@users.noreply.github.com>
2020-12-09 11:30:49 +0900

    routes.js server.js 書き込み

---
+++ express_mvc/routes.js
@@ -0,0 +1,9 @@
+// routesモジュールをエクスポート
+    // routes.jsを読み込んで動作するよう、モジュールをエクスポートしておく。
+    // また Router も利⽤できるようにしておく。
+
+const express = require("express");
+const router = express.Router();
+
+
+module.exports = router;
--- express_mvc/server.js
+++ express_mvc/server.js
@@ -1,13 +1,46 @@
 // モジュール読み込み
 const express = require("express");
-const routes = require("./routes");
 const ejs = require("ejs");
 const config = require("config");
+
+// カスタムモジュール読み込み (このプログラム階層内の相対パス)
+const routes = require("./routes");
+const router = require("./routes");
+    // default.jsonはgitにアップしないので、自作モジュールは手元で管理する
+
 
 // default.json から設定読み込み
 const port = config.server.port;
 const host = config.server.host;
 
+
 // Expressを作成し、ミドルウェアで各設定を有効化
+// Express作成
+const app = express();
+
+// EJS設定
+app.set("ejs", ejs.renderFile);
+
+// JSON
+app.use(express.json());
+
+// URLエンコード
+app.use(express.urlencoded({ extended: true }));
+
+// 静的ファイル有効
+app.use(express.static(__dirname + "/public"));
+
+// ミドルウェアルーティング
+app.use(routes);
 
 
+// ルーティング
+router.get("/", (req, res) => {
+    res.send("Hello Express Router!!");
+});
+
+
+// ポート：3000 サーバ待機
+app.listen(port, host, () => {
+    console.log(`app listen: http://${host}:${port}`);
+});
