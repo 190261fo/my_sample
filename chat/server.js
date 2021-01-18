4dc833812d947bdac756a9b1cc562ce757f37607
190261fo <69778270+190261fo@users.noreply.github.com>
2021-01-18 09:54:39 +0900

    0118_socket追記_chatフォルダ追加

---
+++ chat/server.js
@@ -0,0 +1,78 @@
+//モジュール読み込み
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const config = require('config')
+let users = {};
+
+//静的ファイル有効
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+logout = (socket) => {
+}
+
+fetchUser = (socket) => {
+    if (!users) return;
+    return users[socket.id];
+}
+
+generateToken = () => {
+    let token = Math.random().toString(32).substring(2);
+    return token;
+}
+
+//connection イベント
+io.on('connection', (socket) => {
+    //socket.id の確認
+
+    // client から server のメッセージ
+    socket.on('client_to_server', (data) => {
+    })
+
+    //ログイン処理
+    socket.on('login', (user) => {
+        //user.isConnect = true なら終了
+
+        //トークン発行
+
+        //Socket ID をキーに user を users 配列に登録
+
+        //data の作成
+
+        //送信元の「logined」に、データ送信 emit()
+
+        //送信元以外全てのクライアントの「user_joined」にデータ送信（ブロードキャスト）
+    });
+
+    //スタンプ送受信
+    socket.on('sendStamp', (imageData) => {
+    });
+
+    //ユーザ一覧
+    socket.on('userList', () => {
+        //送信元の show_users に users をデータ送信
+    });
+
+    //ログアウト
+    socket.on('logout', () => {
+        logout(socket);
+    });
+    
+    //切断
+    socket.on('disconnect', () => {
+        console.log('disconnect');
+        logout(socket);
+    });
+
+});
+
+const port = config.server.port;
+const host = config.server.host;
+http.listen(port, host, () => {
+    console.log(`listening on http://${host}:${port}`)
+})
--- node_socket/public/client.js
+++ node_socket/public/client.js
@@ -4,6 +4,7 @@ $(() => {
     // Socket.io 接続
     let socket = io.connect(url);
 
+    // 接続が確立した
     socket.on("connect", () => {
         console.log("connection!!");
         console.log(socket.id);
@@ -11,14 +12,25 @@ $(() => {
     });
 
     // サーバから受信すると処理される
+    // server_to_client は自分で命名 : サーバに合わせる
     socket.on("server_to_client", (data) => {
         console.log(data)
             // ブラウザ開くと出る？(serverのconnection!)
+        
+        // pタグを生成してhtmlを入れる
+        let p = $("<p>").html(data);
+        // id=message にpタグを追加
+        $("#message").append(p);
+
     });
 
-    $("#send").on("click", () => {
+    // id=send のElement(要素)がクリックされたら
+    // $("#send").on("click", () => {
+    $("#send").on("click", function (event) {
         // クライアントからサーバにデータ送信
         let message = "こんにちは";
+
+        // client_to_server は自分で命名 : サーバに合わせる
         socket.emit("client_to_server", message);
             // ブラウザsendボタン押して出る
     });
